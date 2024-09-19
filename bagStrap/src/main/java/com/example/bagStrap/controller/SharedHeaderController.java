3d8a64631c73b9271a1014a54149cfcb697b7ceb
SM Kang <akfnz123@naver.com>
2024-09-19 10:26:57 +0900

    2024-09-19 10시 시작커밋

--- bagStrap/src/main/java/com/example/bagStrap/controller/SharedHeaderController.java
+++ bagStrap/src/main/java/com/example/bagStrap/controller/SharedHeaderController.java
@@ -11,17 +11,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.bagStrap.dao.SharedHeaderService;
+import com.example.bagStrap.model.User;
 import com.google.gson.Gson;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class SharedHeaderController {
 
 	@Autowired
 	SharedHeaderService sharedHeaderService;
+	@Autowired
+	HttpSession session;
 
 	@RequestMapping("/myStudy.do")
 	public String main(Model model) throws Exception {
-		return "/sharedHeader/myStudy";
+		return "/header/myStudy";
+	}
+	@RequestMapping("/myshop/orders.do")
+	public String orders(Model model) throws Exception {
+		return "/header/header_myshop_orders";
 	}
 
 	@RequestMapping(value = "/myStudy.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
@@ -33,5 +42,48 @@ public class SharedHeaderController {
 
 		return new Gson().toJson(resultMap);
 	}
+	
+	@RequestMapping(value = "/sharedHeader.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String sharedHeader(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
+		HashMap<String, Object> resultMap = new HashMap();
+		try {
+			User user = (User) session.getAttribute("user");
+			if(user.getUserNickName() != null) {
+				resultMap.put("isLogin", true);
+				resultMap.put("userNickName", user.getUserNickName());
+				resultMap.put("userId", user.getUserId());
+
+			}
+		} catch(NullPointerException e) {
+			resultMap.put("isLogin", false);
+		}
+
+		return new Gson().toJson(resultMap);
+	}
+	
+	@RequestMapping(value = "/orderList.dox", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String selectOrderList(Model model, @RequestParam HashMap<String, Object> map) throws Exception {
+		HashMap<String, Object> resultMap = new HashMap();
+
+		try {
+			User user = (User) session.getAttribute("user");
+			if(user != null) {
+				map.put("userId", user.getUserId());
+				
+				resultMap = sharedHeaderService.selectOrderList(map);
+			} else {
+
+				resultMap.put("result", true);
+				resultMap.put("message", "로그인하쇼");
+			}
+		} catch(NullPointerException e) {
+			resultMap.put("isLogin", false);
+		}
+
+		return new Gson().toJson(resultMap);
+	}
+	
 
 }
--- bagStrap/src/main/webapp/WEB-INF/shop/shop_home.jsp
+++ bagStrap/src/main/webapp/WEB-INF/shop/shop_home.jsp
@@ -1,1 +1,72 @@
-shophome
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<jsp:include page="/layout/sharedHeader.jsp"></jsp:include>
+	<title>첫번째 페이지</title>
+</head>
+<style>
+	
+	</style>
+<body>
+	<div id="app">
+		<main class="main-container">
+			
+	        <aside class="sidebar">
+	            사이드바
+	        </aside>
+			
+	        <div class="content">
+	            콘텐츠
+				shophome
+	        </div>
+			
+
+
+	    </main>
+
+	</div>
+	<jsp:include page="/layout/footer.jsp"></jsp:include>
+
+</body>
+</html>
+<script>
+	//localStorage.setItem('data', JSON.stringify(data));
+	// JSON.parse(localStorage.getItem('data')).result
+    const app = Vue.createApp({
+        data() {
+            return {
+                name : '${hello}',
+				list : {},
+				codeList : {},
+				selectedCodes : [],
+				
+            };
+        },
+        methods: {
+			
+            fnGetList(){
+				var self = this;
+				var nparmap = {
+					selectedCodes: JSON.stringify(self.selectedCodes)
+				};
+				$.ajax({
+					url:"itemList.dox",
+					dataType:"json",	
+					type : "POST", 
+					data : nparmap,
+					success : function(data) { 
+						console.log(data);
+					}
+				});
+            }
+        },
+        mounted() {
+            var self = this;
+			//self.fnGetList();
+        }
+    });
+    app.mount('#app');
+</script>
